b8dcf1666f0e6e6445e2401bf2f06001b7286e7c
Ayrton Polfliet <ayrton.polfliet@student.odisee.be>
2020-06-15 21:17:32 +0200

    Toevoegen Visual Studio Solution - WPF Business Layer en folders onder dll en wpf

---
+++ MijnReizen.solution/MijnReizen.DLL/DataService/DesignDataService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MijnReizen.DLL.DataService
+{
+    class DesignDataService
+    {
+    }
+}
---
+++ MijnReizen.solution/MijnReizen.DLL/ViewModels/ReisViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MijnReizen.DLL.ViewModels
+{
+    class ReisViewModel
+    {
+    }
+}
